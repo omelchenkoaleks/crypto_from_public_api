f0579a76ec08810819a3a25032c955e5105354de
omelchenkoaleks <omelchenkoaleks@gmail.com>
2024-09-23 07:05:11 +0300

    Get all crypto currency from api with image and symbol and name

--- lib/feature/crypto_list/crypto_list_screen.dart
+++ lib/feature/crypto_list/crypto_list_screen.dart
@@ -1,18 +1,84 @@
+import 'dart:convert';
+
 import 'package:auto_route/auto_route.dart';
-import 'package:crypto_from_public_api/feature/crypto_list/view/crypto_card.dart';
+import 'package:crypto_from_public_api/model/crypto_currency.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 @RoutePage()
-class CryptoListScreen extends StatelessWidget {
+class CryptoListScreen extends StatefulWidget {
   const CryptoListScreen({super.key});
+
+  @override
+  State<CryptoListScreen> createState() => _CryptoListScreenState();
+}
+
+class _CryptoListScreenState extends State<CryptoListScreen> {
+  late Future<List<CryptoCurrency>> cryptoList;
+
+  @override
+  void initState() {
+    super.initState();
+    cryptoList = fetchCryptoCurrencies();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => const CryptoCard(),
+      appBar: AppBar(title: const Text('Crypto List')),
+      body: FutureBuilder<List<CryptoCurrency>>(
+        future: cryptoList,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Ошибка: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('Список пуст'));
+          } else {
+            final cryptos = snapshot.data!;
+            return ListView.builder(
+              itemCount: cryptos.length,
+              itemBuilder: (context, index) {
+                final crypto = cryptos[index];
+                return ListTile(
+                  leading: Image.network(
+                    crypto.imageUrl,
+                    width: 40,
+                    height: 40,
+                    errorBuilder: (context, error, stackTrace) {
+                      return const Icon(Icons.error);
+                    },
+                  ),
+                  title: Text(crypto.name),
+                  subtitle: Text(crypto.symbol),
+                );
+              },
+            );
+          }
+        },
       ),
     );
+  }
+}
+
+Future<List<CryptoCurrency>> fetchCryptoCurrencies() async {
+  const url = 'https://min-api.cryptocompare.com/data/all/coinlist';
+
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> json = jsonDecode(response.body);
+
+    final Map<String, dynamic> data = json['Data'];
+    final List<CryptoCurrency> cryptoList = [];
+
+    data.forEach((key, value) {
+      cryptoList.add(CryptoCurrency.fromJson(value));
+    });
+
+    return cryptoList;
+  } else {
+    throw Exception('Не удалось загрузить список валют');
   }
 }
---
+++ lib/model/crypto_currency.dart
@@ -0,0 +1,19 @@
+class CryptoCurrency {
+  final String symbol;
+  final String name;
+  final String imageUrl;
+
+  CryptoCurrency({
+    required this.symbol,
+    required this.name,
+    required this.imageUrl,
+  });
+
+  factory CryptoCurrency.fromJson(Map<String, dynamic> json) {
+    return CryptoCurrency(
+      symbol: json['Symbol'],
+      name: json['CoinName'],
+      imageUrl: 'https://www.cryptocompare.com${json['ImageUrl']}',
+    );
+  }
+}
